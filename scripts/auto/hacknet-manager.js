3037f4050058ad400c636ac06f89fa56cdbbee0d
Zamara <alexiszamara@gmail.com>
2022-02-22 02:40:20 +0100

    added hacket/server managers

---
+++ scripts/auto/hacknet-manager.js
@@ -0,0 +1,70 @@
+/** @param {NS} ns **/
+const home = "home";
+
+export async function main(ns) {
+	let cooldown = 50;
+
+	while(true) {
+		let node;
+		const totalNodes = await ns.hacknet.numNodes();
+
+		let moneyAvailable = await ns.getServerMoneyAvailable(home);
+		let maxCost = Number.MAX_SAFE_INTEGER;
+		let cheapestPart;
+		let buyNextNode = false;
+
+		if(totalNodes > 0) {
+			for(let i = 0; i < totalNodes; i++) {
+				if(ns.hacknet.getLevelUpgradeCost(i,1) < maxCost) {
+					maxCost = ns.hacknet.getLevelUpgradeCost(i,1);
+					cheapestPart = 0;
+					node = i;
+				}
+				if(ns.hacknet.getRamUpgradeCost(i,1) < maxCost) {
+					maxCost = ns.hacknet.getRamUpgradeCost(i,1);
+					cheapestPart = 1;
+					node = i;
+				}
+				if(ns.hacknet.getCoreUpgradeCost(i,1) < maxCost) {
+					maxCost = ns.hacknet.getCoreUpgradeCost(i,1);
+					cheapestPart = 2;
+					node = i;
+				}
+			}
+			if(ns.hacknet.getPurchaseNodeCost() < maxCost) {
+				buyNextNode = true;
+				maxCost = ns.hacknet.getPurchaseNodeCost();
+			}
+			
+			if(moneyAvailable > maxCost) {
+				if(buyNextNode) await ns.hacknet.purchaseNode();
+				else {
+					switch(cheapestPart) {
+						case 0:
+							await ns.hacknet.upgradeLevel(node,1);
+							break;
+						case 1:
+							await ns.hacknet.upgradeRam(node,1);
+							break;
+						case 2:
+							await ns.hacknet.upgradeCore(node,1);
+							break;
+						default:
+							await ns.toast("hacknet-manager.js: illegal cheapestPart value. Terminating.","error",null);
+							ns.exit();
+					}
+				}
+				cooldown = 50;
+			}
+			else cooldown += 5000;
+		}
+		else {
+			if(moneyAvailable > ns.hacknet.getPurchaseNodeCost()) {
+				await ns.hacknet.purchaseNode();
+				cooldown = 50;
+			}
+			else cooldown += 5000;
+		}
+		await ns.sleep(cooldown);
+	}
+}
--- scripts/auto/server-manager.js
+++ scripts/auto/server-manager.js
@@ -1,8 +1,9 @@
 /** @param {NS} ns **/
 var ns;
 
-var home = "home";
-var remoteHackFile = "/scripts/remote-hack-boost.js";
+const home = "home";
+const ownedFiles = ["/scripts/batching/coordinator.js","/scripts/batching/hack.js","/scripts/batching/grow.js","/scripts/batching/weaken.js"];
+const hwgwCoordinator = "scripts/batching/coordinator.js";
 
 export async function main(_ns) {
 	ns = _ns;
@@ -45,8 +46,7 @@ export async function main(_ns) {
 						await ns.killall(serverName);
 						await ns.deleteServer(serverName);
 						await ns.purchaseServer(serverName, Math.min(Math.pow(base,power),serverRamLimit));
-						ns.print("purchased new server: " + serverName + " with " + ns.getServerMaxRam(serverName) + "GB of RAM");
-						await restartHackBooster(serverName);
+						await restartServices(serverName);
 						cooldown = 100000;
 					}
 					cooldown += 50000;
@@ -77,8 +77,7 @@ export async function main(_ns) {
 					}
 				}
 				await ns.purchaseServer(serverName, Math.min(Math.pow(base,power),serverRamLimit));
-				ns.print("purchased new server: " + serverName + " with " + ns.getServerMaxRam(serverName) + "GB of RAM");
-				await restartHackBooster(serverName);
+				await restartServices(serverName);
 				cooldown = 100000;
 			}
 			else {
@@ -87,14 +86,12 @@ export async function main(_ns) {
 		}
 		await ns.sleep(cooldown);
 	}
-	ns.toast("server-manager.js: all servers maxed out.","success",null);
+	ns.toast("server-manager.js: all servers RAM maxed out.","success",null);
 }
 
-async function restartHackBooster(targetServer) {
-	let serverToHack = ns.ps("n00dles")[0].args[0];
-	let threads = Math.floor(await ns.getServerMaxRam(targetServer) / await ns.getScriptRam(remoteHackFile));
-	if(threads == 0) return true;
-	ns.print("found target to hack from n00dles: " + serverToHack + " using " + threads + " threads on " + targetServer);
-	await ns.scp(remoteHackFile, home, targetServer);
-	await ns.exec(remoteHackFile, targetServer, threads, serverToHack);
+async function restartServices(targetServer) {
+	for(file of ownedFiles) {
+		await ns.scp(file, home, targetServer);
+	}
+	await ns.exec(hwgwCoordinator,serverName);
 }
