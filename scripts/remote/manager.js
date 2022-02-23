c2f8e626f52c2c1963aad4499bb5abf8b652e675
Zamara <alexiszamara@gmail.com>
2022-02-23 14:29:41 +0100

    fixed sleep()
    sleep() was taking Math.max of weakenTime and growTime as if they ran simultaneous.
    now takes weakenTime + growTime because they run consecutively.

--- scripts/remote/manager.js
+++ scripts/remote/manager.js
@@ -4,6 +4,6 @@ export async function main(ns) {
 		let thread = Math.floor((ns.getServerMaxRam(ns.getHostname()) - ns.getServerUsedRam(ns.getHostname())) / ns.getScriptRam("/scripts/remote/self-weaken-grow.js"));
 		thread = thread > 1 ? thread : 1;
 		ns.run("/scripts/remote/self-weaken-grow.js",thread,ns.getHostname());
-		await ns.sleep(Math.max(ns.getWeakenTime(ns.getHostname())),ns.getGrowTime(ns.getHostname()));
+		await ns.sleep(Math.ceil(ns.getWeakenTime(ns.getHostname())) + ns.getGrowTime(ns.getHostname()));
 	}
 }
